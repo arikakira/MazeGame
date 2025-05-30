aafec7f4ffab5bba58327e01e3eaad016e5dd3c6
Sam <samanthaverrazano@gmail.com>
2025-05-29 21:56:54 -0400

    arrows are sprites now

--- core/src/main/java/io/github/arikakira/MazeGame/Main.java
+++ core/src/main/java/io/github/arikakira/MazeGame/Main.java
@@ -8,31 +8,46 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.math.Vector2;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
-    private SpriteBatch batch;
-    private Texture image;
-    private Texture rArrowTexture;
-    private Texture lArrowTexture;
-    private Texture uArrowTexture;
-    private Texture dArrowTexture;
+    private Texture arrowTexture;
+    // private Texture lArrowTexture;
+    // private Texture uArrowTexture;
+    // private Texture dArrowTexture;
 
     private SpriteBatch spriteBatch;
     private FitViewport viewport;
 
+    private Sprite rArrowSprite;
+    private Sprite lArrowSprite;
+    private Sprite uArrowSprite;
+    private Sprite dArrowSprite;
+
+    Vector2 touchPos;
+
     @Override
     public void create() {
-        batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
-
         spriteBatch = new SpriteBatch();
-        rArrowTexture = new Texture("rightArrow.png");
-        lArrowTexture = new Texture("leftArrow.png");
-        uArrowTexture = new Texture("upArrow.png");
-        dArrowTexture = new Texture("downArrow.png");
+        arrowTexture = new Texture("arrow.png");
+        // lArrowTexture = new Texture("leftArrow.png");
+        // uArrowTexture = new Texture("upArrow.png");
+        // dArrowTexture = new Texture("downArrow.png");
         viewport = new FitViewport(9, 7);
+
+        rArrowSprite = new Sprite(arrowTexture);
+        rArrowSprite.setSize(2, 1);
+        lArrowSprite = new Sprite(arrowTexture);
+        lArrowSprite.setSize(2, 1);
+        uArrowSprite = new Sprite(arrowTexture);
+        uArrowSprite.setSize(2, 1);
+        dArrowSprite = new Sprite(arrowTexture);
+        dArrowSprite.setSize(2, 1);
+
+        touchPos = new Vector2();
     }
 
     @Override
@@ -53,7 +68,9 @@ public class Main extends ApplicationAdapter {
     }
 
     public void input() {
-
+        if(Gdx.input.isTouched()) {
+            // does something when user cicks the screen
+        }
     }
 
     public void logic() {
@@ -65,16 +82,32 @@ public class Main extends ApplicationAdapter {
         viewport.apply();
         spriteBatch.setProjectionMatrix(viewport.getCamera().combined);
         spriteBatch.begin();
-        spriteBatch.draw(rArrowTexture, 7,3,2,1);
-        spriteBatch.draw(lArrowTexture, 0,3,2,1);
-        spriteBatch.draw(uArrowTexture, 4,5,1,2);
-        spriteBatch.draw(dArrowTexture, 4,0,1,2);
+
+        //spriteBatch.draw(rArrowTexture, 7,3,2,1);
+        rArrowSprite.draw(spriteBatch);
+        rArrowSprite.setPosition(7, 3);
+        // spriteBatch.draw(lArrowTexture, 0,3,2,1);
+        lArrowSprite.draw(spriteBatch);
+        lArrowSprite.setOrigin(lArrowSprite.getWidth()/2f, lArrowSprite.getHeight()/2f);
+        lArrowSprite.setRotation(180);
+        lArrowSprite.setPosition(0, 3);
+        //spriteBatch.draw(uArrowTexture, 4,5,1,2);
+        uArrowSprite.draw(spriteBatch);
+        uArrowSprite.setOrigin(uArrowSprite.getWidth()/2f, uArrowSprite.getHeight()/2f);
+        uArrowSprite.setRotation(90);
+        uArrowSprite.setPosition((float) 3.5, (float) 5.5);
+        // spriteBatch.draw(dArrowTexture, 4,0,1,2);
+        dArrowSprite.draw(spriteBatch);
+        dArrowSprite.setOrigin(dArrowSprite.getWidth()/2f, dArrowSprite.getHeight()/2f);
+        dArrowSprite.setRotation(270);
+        dArrowSprite.setPosition((float) 3.5, (float) 0.5);
+
         spriteBatch.end();
     }
 
     @Override
     public void dispose() {
-        batch.dispose();
-        image.dispose();
+        spriteBatch.dispose();
+        arrowTexture.dispose();
     }
 }
